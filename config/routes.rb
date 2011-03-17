3e42faa9f77f540d069ce865de6f891c3c89cd6f
Yomi Colledge <baphled@boodah.net>
2011-03-17 17:29:07 +0000

    Added integration test for following people

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match '/tweet-stack' => "tweet_stack#index", :as => :tweet_stack
   match '/tweet-stack/search' => "tweet_stack#search", :as => :tweet_search_phrase
+  match '/tweet-stack/follow' => "tweet_stack#follow", :as => :tweet_follow
 end
--- spec/integration/navigation_spec.rb
+++ spec/integration/navigation_spec.rb
@@ -14,8 +14,8 @@ describe "Navigation" do
   
   it "should let me do a search based on a phrase" do
     stub_request(:get, "https://search.twitter.com/search.json?q=lemons&rpp=100").
-             with(:headers => {'Accept'=>'application/json', 'User-Agent'=>'Twitter Ruby Gem 1.1.2'}).
-             to_return(:status => 200, :body => fixture('search.json'), :headers => {})
+      with(:headers => {'Accept'=>'application/json', 'User-Agent'=>'Twitter Ruby Gem 1.1.2'}).
+      to_return(:status => 200, :body => fixture('search.json'), :headers => {})
     visit "/tweet-stack"
     page.should_not have_content "Tweeple talking about 'lemons':"
     fill_in "Search for:", :with => "lemons"
@@ -23,7 +23,23 @@ describe "Navigation" do
     page.should have_content "Tweeple talking about 'lemons':"
   end
   
-  it "allows me to follow a batch of tweeples"
+  it "allows me to follow a batch of tweeples" do
+    stub_request(:get, "https://search.twitter.com/search.json?q=lemons&rpp=100").
+      with(:headers => {'Accept'=>'application/json', 'User-Agent'=>'Twitter Ruby Gem 1.1.2'}).
+      to_return(:status => 200, :body => fixture('search.json'), :headers => {})
+    visit "/tweet-stack"
+    fill_in "Search for:", :with => "lemons"
+    click_button "Search"
+    
+    #select all tweeple found
+    check "tweet_stack[1]"
+    # press follow
+    click_button "Follow"
+    
+    # should see a list of users that I am now following
+    page.should have_content "You are now following killermelons"
+  end
+  
   it "should be able to store a the found tweeple for later"
   it "tweeple I am following should be removed from the to add list"
   it "it alls me to stack up tweets"
