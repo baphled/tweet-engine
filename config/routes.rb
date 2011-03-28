90dd5e1ae8ca09c0c3e3806a59bff1013a702487
Yomi Colledge <baphled@boodah.net>
2011-03-29 00:45:45 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   match '/tweet-stack' => "tweet_stack#index", :as => :tweet_stack
   match '/tweet-stack/search' => "tweet_stack#search", :as => :tweet_search_phrase
   match '/tweet-stack/follow' => "tweet_stack#follow", :as => :tweet_follow
-  match '/tweet-stack/followers' => "tweet_stack#followers", :as => :tweet_stack_followers
+  match '/tweet-stack/following' => "tweet_stack#following", :as => :tweet_stack_following
   match '/tweet-stack/unfollow' => "tweet_stack#unfollow", :as => :tweet_stack_unfollow
   match '/tweet-stack/stack' => "tweet_stack#stack", :as => :stack_tweet
   
