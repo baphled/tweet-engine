8cc03991baf500a6b568bd98518649be2097fb6f
Yomi Colledge <baphled@boodah.net>
2011-03-29 23:25:08 +0100

    Cleanup

--- lib/generators/initializer/templates/initializer.rb
+++ lib/generators/initializer/templates/initializer.rb
@@ -1,2 +1,9 @@
 # Add initialization content here
 TweetStack.config = YAML.load_file("#{Rails.root}/config/tweet_stack.yml")[Rails.env]
+
+Twitter.configure do |config|
+  config.consumer_key = "CK"
+  config.consumer_secret = "CS"
+  config.oauth_token = "OT"
+  config.oauth_token_secret = "OTS"
+end
