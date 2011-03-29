4ce8769e579fd32aaf8daf6abd2b85c9e8be817d
Yomi Colledge <baphled@boodah.net>
2011-03-30 00:55:53 +0100

    Trying to resolve dependency issues

--- lib/generators/initializer/initializer_generator.rb
+++ lib/generators/initializer/initializer_generator.rb
@@ -1,6 +1,14 @@
 class InitializerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
- 
+  
+  gem 'mongoid', :git => "https://github.com/mongoid/mongoid.git"
+  gem 'bson_ext'
+  gem 'simple_form'
+  gem 'jquery-rails', '>= 0.2.6'
+  gem 'delayed_job', '>= 2.1.4', :git => "https://github.com/collectiveidea/delayed_job"
+  gem 'delayed_job_mongoid', :git => "https://github.com/collectiveidea/delayed_job_mongoid.git"
+  gem 'djinn'
+  
   def copy_initializer_file
     copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
     copy_file "script/tweet_engine_runner", "script/#{file_name}"
