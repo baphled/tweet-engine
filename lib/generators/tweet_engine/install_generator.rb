eddffbf4b4c619370ac17b4464a432b3284fdbcb
Yomi Colledge <baphled@boodah.net>
2011-03-31 00:38:13 +0100

    Setup so that we don't need to pass an name to the generator anymore.

--- lib/generators/tweet_engine/install_generator.rb
+++ lib/generators/tweet_engine/install_generator.rb
@@ -1,18 +1,18 @@
 module TweetEngine
   module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
+    class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
       
       def copy_initializer
-        template "initializer.rb", "config/initializers/#{file_name}.rb"
+        template "initializer.rb", "config/initializers/tweet_engine.rb"
       end
       
       def copy_script
-        copy_file "script/tweet_engine_runner", "script/#{file_name}"
+        copy_file "script/tweet_engine_runner", "script/tweet_engine"
       end
       
       def copy_config
-        copy_file "config/tweet_engine.yml", "config/#{file_name}.yml"
+        copy_file "config/tweet_engine.yml", "config/tweet_engine.yml"
       end
     end
   end
