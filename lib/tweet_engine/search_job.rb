e1ae657cf142d321a1c7022ea68c5c281a2e9d8f
Yomi Colledge <baphled@boodah.net>
2011-03-31 15:13:54 +0100

    Added documentation

--- lib/tweet_engine/search_job.rb
+++ lib/tweet_engine/search_job.rb
@@ -1,26 +1,45 @@
 require 'delayed_job'
 require 'delayed_job_mongoid'
 
+
 module TweetEngine
+  #
+  # A Searching process to help people to find potential followers
+  #
+  #
   class SearchJob
     
+    #
+    # Searches for potential people to follow
+    #
+    # As this process can take a while we run it in the background.
+    #
+    # To find potential followers on Twitter we need to run a search periodically
+    # to do this we need to run a daemon process that calls searching periodically
+    #
     def searching
       users = TweetEngine.search TweetEngine.config['keywords']
-      users_to_add gathered_names(users)
+      store_search gather_potentials(users)
     end
     
     handle_asynchronously :searching
     
     protected
     
-    def gathered_names users
+    #
+    # Collects a list of potential people to follow
+    #
+    def gather_potentials users
       names = []
       all_users = TweetEngine::PotentialFollower.all.to_a
       users.each { |user| names << user.from_user unless all_users.include? user.from_user }
       names
     end
     
-    def users_to_add names
+    #
+    # Stores the list of potential followers
+    #
+    def store_search names
       all_users = TweetEngine::PotentialFollower.all.to_a
       names.each do |user|
         TweetEngine::PotentialFollower.create!(:screen_name => user) unless all_users.include? user
