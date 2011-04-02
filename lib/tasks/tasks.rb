3b741e98f76fb11b78d7fa46d0a44bb7015f0c8b
Yomi Colledge <baphled@boodah.net>
2011-04-02 19:21:05 +0100

    Updated so that we run delayed_job as well as our runner

--- lib/tasks/tasks.rb
+++ lib/tasks/tasks.rb
@@ -3,10 +3,9 @@
 TweetEngine.config = YAML.load_file("#{Rails.root}/config/tweet_engine.yml")[Rails.env]
 
 namespace :tweet_engine do
-  desc "Start a tweet_engine daemon."
+  desc "Start a tweet_engine and delayed_job daemon."
   task :runner do
-    TweetEngine::Runner.djinnify_rails do |djinn|
-      puts "oh my djinn's"
-    end
+    TweetEngine::Runner.djinnify_rails
+    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
   end
 end
