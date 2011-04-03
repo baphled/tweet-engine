8854b85fb3c7f03af1ff3ecf0fea4e77909906dc
Yomi Colledge <baphled@boodah.net>
2011-04-03 01:36:06 +0100

    Removed commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,4 @@ Rails.application.routes.draw do
   end
   
   match '/tweet-engine' => "tweet_engine/engine#index", :as => :tweet_engine
-  # match '/tweet-engine/search' => "tweet_engine#search", :as => :tweet_search_phrase
-  # match '/tweet-engine/follow' => "tweet_engine#follow", :as => :tweet_follow
-  # match '/tweet-engine/following' => "tweet_engine#following", :as => :tweet_engine_following
-  # match '/tweet-engine/followers' => "tweet_engine#followers", :as => :tweet_engine_followers
-  # match '/tweet-engine/unfollow' => "tweet_engine#unfollow", :as => :tweet_engine_unfollow
-  # match '/tweet-engine/potential-followers' => "tweet_engine#potential_followers", :as => :tweet_engine_potential_followers
-  # match '/tweet-engine/stack' => "tweet_engine#stack", :as => :stack_tweet
-  # 
-  # resources :tweet, :path => "tweet-engine/tweet"
 end
