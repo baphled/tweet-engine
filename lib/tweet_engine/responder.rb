5f6a09391db15d22bae917f918e00b7f491d4d8e
Yomi Colledge <baphled@boodah.net>
2011-04-12 21:37:55 +0100

    Refactored and added validations to make sure that responses and key_phrases are entered

--- lib/tweet_engine/responder.rb
+++ lib/tweet_engine/responder.rb
@@ -7,22 +7,34 @@ module TweetEngine
     field :response
     field :sent_to, :type => Array, :default => []
     
+    validates_presence_of :key_phrases, :response
+    
     class << self
       def respond
-        users_reponded_to = []
-        items = self.all.to_a
-        items.each do |item|
-          tweets = TweetEngine.search item.key_phrases
-          tweets.each do |tweet|
-            unless item.sent_to.include? tweet.from_user
-              TweetEngine::Stack.create! :message => "@#{tweet.from_user} #{item.response}"
-              item.sent_to << tweet.from_user
-            end
-          end
-          item.save
-          users_reponded_to.concat item.sent_to
+        @users_reponded_to = []
+        auto_responses = self.all.to_a
+        search_for_respondees auto_responses
+        @users_reponded_to
+      end
+      
+      protected
+      
+      def search_for_respondees auto_responses
+        auto_responses.each do |auto_response|
+          tweets = TweetEngine.search auto_response.key_phrases
+          send_response tweets, auto_response
         end
-        users_reponded_to
+      end
+      
+      def send_response tweets, auto_response
+        tweets.each do |tweet|
+          unless auto_response.sent_to.include? tweet.from_user
+            TweetEngine::Stack.create! :message => "@#{tweet.from_user} #{auto_response.response}"
+            auto_response.sent_to << tweet.from_user
+          end
+        end
+        auto_response.save
+        @users_reponded_to.concat auto_response.sent_to
       end
     end
   end
